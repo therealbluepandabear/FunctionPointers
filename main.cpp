07367e5c76b413111bc8ad057a570f8ab1ff1e9f
xTheCoderx <justaguyoney@gmail.com>
2021-10-17 16:21:02 +1300

    In C, there is a function called ‘atexit’ which is used to invoke a function just before your program terminates. I have tried that our here with the function 'EndMessage()'.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,18 @@ void Print(int count, char ch) {
     std::cout << std::endl;
 }
 
+void EndMessage() {
+    std::cout << "End of program." << std::endl;
+}
+
 int main() {
     Print(5, '#');
 
     void (*pfn) (int, char) = &Print;
     (*pfn)(8, '#');
     pfn(5, '#');
+
+    atexit(&EndMessage);
 
     return 0;
 }
