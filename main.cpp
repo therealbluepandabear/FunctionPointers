d36ac3b409f12870cef6e26a4cc6bc642582638b
xTheCoderx <justaguyoney@gmail.com>
2021-10-17 16:11:39 +1300

    Added Print() function which we can later use to test function pointers in C++.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+void Print(int count, char ch) {
+    for (int i = 0; i < count; i++) {
+        std::cout << ch;
+    }
+    std::cout << std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
